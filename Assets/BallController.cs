fcbb292ef9a02eece91e0c5cbdb0d377aa3578df
HarjotSahota <93726984+HarjotSahota@users.noreply.github.com>
2025-01-28 16:48:46 -0800

    Added ball movement and speed

---
+++ Assets/BallController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+    // to use rigidbody componenets
+    public Rigidbody sphereRigidbody;
+    // to change speed
+    public float ballSpeed = 2f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // prints out console message when Start method is called
+        Debug.Log("Calling the Start method");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector2 inputVector = Vector2.zero;          // initialize input vector
+
+        if (Input.GetKey(KeyCode.W)){
+            inputVector += Vector2.up;
+            Debug.Log(Vector2.up);
+        }
+        if (Input.GetKey(KeyCode.S)){
+            inputVector += Vector2.down;
+            Debug.Log(Vector2.down);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            inputVector += Vector2.right;
+            Debug.Log(Vector2.right);
+        }
+        if (Input.GetKey(KeyCode.A)){
+            inputVector += Vector2.left;
+            Debug.Log(Vector2.left);
+        }
+
+        // allows input to control movement on the desired plane
+        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        Debug.Log("Resultant Vector: " + inputVector);
+        Debug.Log("Resultant 3D Vector: " + inputXZPlane);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+        
+
+
+
+        
+    }
+}
